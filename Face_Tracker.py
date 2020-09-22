69a47f82a62b047fc401b6cad1a6e287c9e8b154
Patrick Martins <pattrickpx99@gmail.com>
2020-09-22 14:41:38 -0300

    click test

--- Face_Tracker.py
+++ Face_Tracker.py
@@ -10,12 +10,14 @@ from IPython.display import  clear_output, Image, display
 from PIL import Image as Img
 from pynput.mouse import Controller, Button
 
-min_olho_esquerdo,min_olho_direito= (100,100)
+min_olho_esquerdo,min_olho_direito,= (100,100)
 x_n=[]
 y_n=[]
+ar_olho_esq_n =[]
+ar_olho_dir_n =[]
 # FACE = list(range(17, 68))
 # FACE_COMPLETA = list(range(0, 68))
-# LABIO = list(range(48, 61))
+LABIO = list(range(48, 61))
 # SOMBRANCELHA_DIRETA = list(range(17, 22))
 # SOMBRANCELHA_ESQUERDA = list(range(22, 27))
 OLHO_DIREITO = list(range(36,42))
@@ -44,10 +46,18 @@ def aspecto_razao_olhos(pontos_olhos):
     aspecto_razao = (a + b)/(2.0 * c)
     
     return aspecto_razao
-
+def aspecto_razao_boca(pontos_boca):
+    
+    a = dist.euclidean(pontos_boca[3], pontos_boca[9])
+    b = dist.euclidean(pontos_boca[2], pontos_boca[10])
+    c = dist.euclidean(pontos_boca[4], pontos_boca[8])
+    d = dist.euclidean(pontos_boca[0], pontos_boca[6])
+    aspecto_razao = (a + b + c)/(3.0 * d)
+    
+    return aspecto_razao
 def anotar_marcos_casca_convexa(imagem, marcos):
     retangulos = detector_face(imagem, 1)
-    global min_olho_esquerdo,min_olho_direito,x_n,y_n
+    global min_olho_esquerdo,min_olho_direito,x_n,y_n,ar_olho_dir_n,ar_olho_esq_n
 
     if len(retangulos) == 0:
         return None
@@ -59,35 +69,41 @@ def anotar_marcos_casca_convexa(imagem, marcos):
     p2=[]
     p3=[]
     p4=[]
+
+    for idx, ret in enumerate(retangulos):
+        marco = marcos[idx]
+        
+        pontos = cv2.convexHull(marco[LABIO])
+        cv2.drawContours(imagem, [pontos], 0, (0,255,0), 1)
     for marco in marcos:
         for idx,ponto in enumerate(marco):
             # print(ponto)
             centro =(ponto[0,0],ponto[0,1])
             if idx==0:
                 p=ponto
-                cv2.circle(imagem,centro,3,(255,255,0),-1)
-                cv2.putText(imagem,str(idx),centro, cv2.FONT_HERSHEY_SIMPLEX,0.3,(255,255,255), 1)
+                # cv2.circle(imagem,centro,3,(255,255,0),-1)
+                # cv2.putText(imagem,str(idx),centro, cv2.FONT_HERSHEY_SIMPLEX,0.3,(255,255,255), 1)
             if idx==27:
                 p1=ponto
-                cv2.circle(imagem,centro,3,(255,255,0),-1)
-                cv2.putText(imagem,str(idx),centro, cv2.FONT_HERSHEY_SIMPLEX,0.3,(255,255,255), 1)
+                # cv2.circle(imagem,centro,3,(255,255,0),-1)
+                # cv2.putText(imagem,str(idx),centro, cv2.FONT_HERSHEY_SIMPLEX,0.3,(255,255,255), 1)
             if idx==16:
                 p2=ponto
-                cv2.circle(imagem,centro,3,(255,255,0),-1)
-                cv2.putText(imagem,str(idx),centro, cv2.FONT_HERSHEY_SIMPLEX,0.3,(255,255,255), 1)
+                # cv2.circle(imagem,centro,3,(255,255,0),-1)
+                # cv2.putText(imagem,str(idx),centro, cv2.FONT_HERSHEY_SIMPLEX,0.3,(255,255,255), 1)
             if idx==8:
                 p3=ponto
-                cv2.circle(imagem,centro,3,(255,255,0),-1)
-                cv2.putText(imagem,str(idx),centro, cv2.FONT_HERSHEY_SIMPLEX,0.3,(255,255,255), 1)
+                # cv2.circle(imagem,centro,3,(255,255,0),-1)
+                # cv2.putText(imagem,str(idx),centro, cv2.FONT_HERSHEY_SIMPLEX,0.3,(255,255,255), 1)
             if idx==30:
                 p4=ponto
-                cv2.circle(imagem,centro,3,(255,255,0),-1)
-                cv2.putText(imagem,str(idx),centro, cv2.FONT_HERSHEY_SIMPLEX,0.3,(255,255,255), 1)
+                # cv2.circle(imagem,centro,3,(255,255,0),-1)
+                # cv2.putText(imagem,str(idx),centro, cv2.FONT_HERSHEY_SIMPLEX,0.3,(255,255,255), 1)
    
-    cv2.line(imagem,(p[0,0],p[0,1]),(p1[0,0],p1[0,1]),(0,255,0), 1)
-    cv2.line(imagem,(p1[0,0],p1[0,1]),(p2[0,0],p2[0,1]),(0,255,0), 1)
-    cv2.line(imagem,(p1[0,0],p1[0,1]),(p4[0,0],p4[0,1]),(0,255,0), 1)
-    cv2.line(imagem,(p4[0,0],p4[0,1]),(p3[0,0],p3[0,1]),(0,255,0), 1)
+    # cv2.line(imagem,(p[0,0],p[0,1]),(p1[0,0],p1[0,1]),(0,255,0), 1)
+    # cv2.line(imagem,(p1[0,0],p1[0,1]),(p2[0,0],p2[0,1]),(0,255,0), 1)
+    # cv2.line(imagem,(p1[0,0],p1[0,1]),(p4[0,0],p4[0,1]),(0,255,0), 1)
+    # cv2.line(imagem,(p4[0,0],p4[0,1]),(p3[0,0],p3[0,1]),(0,255,0), 1)
     x= np.linalg.norm(p2-p)
     x= np.linalg.norm(p1-p)/x
     xleft=0.3
@@ -97,8 +113,8 @@ def anotar_marcos_casca_convexa(imagem, marcos):
     y=np.linalg.norm(p4-p1)/y
     ytop=0.19
     ybottom=0.35
-    cv2.putText(imagem,f'x= {round(x,3)}',(10,10), cv2.FONT_HERSHEY_SIMPLEX,0.4,(255,255,255), 1)
-    cv2.putText(imagem,f'y= {round(y,3)}',(10,30), cv2.FONT_HERSHEY_SIMPLEX,0.4,(255,255,255), 1)
+    # cv2.putText(imagem,f'x= {round(x,3)}',(10,10), cv2.FONT_HERSHEY_SIMPLEX,0.4,(255,255,255), 1)
+    # cv2.putText(imagem,f'y= {round(y,3)}',(10,30), cv2.FONT_HERSHEY_SIMPLEX,0.4,(255,255,255), 1)
     
     if x<xleft:
         x=xleft
@@ -114,7 +130,7 @@ def anotar_marcos_casca_convexa(imagem, marcos):
     y= int((y-ytop)/(ybottom-ytop)*1024)
     mouse = Controller()
     # print(x)
-    media_movel=3
+    media_movel=20
     if(len(x_n)>media_movel):
         x_n.pop(0)
         x_n.append(x)
@@ -148,18 +164,51 @@ def anotar_marcos_casca_convexa(imagem, marcos):
     #     print('b: ',b1,' | ',b2)
     #     print()
     # test(marcos[0][OLHO_DIREITO],marcos[0][OLHO_ESQUERDO])
-    ar_olho_dir = round(aspecto_razao_olhos(marcos[0][OLHO_ESQUERDO]),3)
-    ar_olho_esq = round(aspecto_razao_olhos(marcos[0][OLHO_DIREITO]),3)
-    if ar_olho_esq< min_olho_esquerdo:
-        min_olho_esquerdo=ar_olho_esq
-    if ar_olho_dir< min_olho_direito:
-        min_olho_direito=ar_olho_dir
-    info_oe =f'olho dir: {ar_olho_esq} minimo: {min_olho_esquerdo}'
-    info_od =f'olho esq: {ar_olho_dir} minimo: {min_olho_direito}'
-    cv2.putText(imagem,info_oe,(5,50), cv2.FONT_HERSHEY_SIMPLEX,0.4,(255,255,255), 1)
-    cv2.putText(imagem,info_od,(10,80), cv2.FONT_HERSHEY_SIMPLEX,0.4,(255,255,255), 1)
+    # ar_olho_dir = round(aspecto_razao_olhos(marcos[0][OLHO_ESQUERDO]),3)
+    # ar_olho_esq = round(aspecto_razao_olhos(marcos[0][OLHO_DIREITO]),3)
+    boca = round(aspecto_razao_boca(marcos[0][LABIO]),3)
+    # if ar_olho_esq< min_olho_esquerdo:
+    #     min_olho_esquerdo=ar_olho_esq
+    # if ar_olho_dir< min_olho_direito:
+    #     min_olho_direito=ar_olho_dir
+    # info_oe =f'olho dir: {ar_olho_esq} minimo: {min_olho_esquerdo}'
+    # info_od =f'olho esq: {ar_olho_dir} minimo: {min_olho_direito}'
+    # print(info_oe)
+    # print(info_od)
+    # cv2.putText(imagem,info_oe,(5,50), cv2.FONT_HERSHEY_SIMPLEX,0.4,(255,255,255), 1)
+    # cv2.putText(imagem,info_od,(10,80), cv2.FONT_HERSHEY_SIMPLEX,0.4,(255,255,255), 1)
+#   
+    
+    # if(len(ar_olho_dir_n)>media_movel):
+    #     ar_olho_dir_n.pop(0)
+    #     ar_olho_dir_n.append(ar_olho_dir)
+    # else:
+    #     ar_olho_dir_n.append(ar_olho_dir)
+    # ar_olho_dir_s=0
 
-    if (ar_olho_dir>0.19 and ar_olho_esq<0.19):
+    # for i in ar_olho_dir_n:
+    #     ar_olho_dir_s+=i
+
+    # ar_olho_dir= ar_olho_dir_s/len(ar_olho_dir_n)
+    
+    # if(len(ar_olho_esq_n)>media_movel):
+    #     ar_olho_esq_n.pop(0)
+    #     ar_olho_esq_n.append(ar_olho_esq)
+    # else:
+    #     ar_olho_esq_n.append(ar_olho_esq)
+    # ar_olho_esq_s=0
+
+    # for i in ar_olho_esq_n:
+    #     ar_olho_esq_s+=i
+
+    # ar_olho_esq= ar_olho_esq_s/len(ar_olho_esq_n)
+
+    # info_oe =f'olho dir: {ar_olho_esq} olho esq: {ar_olho_dir}'
+    
+    print(boca)
+   
+    # if (ar_olho_dir>0.19 and ar_olho_esq<0.15):
+    if (boca>0.6):
         mouse.click(Button.left)
     return imagem
 
@@ -183,10 +232,11 @@ try:
     while(True):
         captura_ok, frame = captura.read()
         if  captura_ok:
-            frame = frame[100:502, 100:502]
+            frame = frame[100:500, 100:500]
             frame=cv2.flip(frame,1)
+            frame = cv2.resize(frame, (100, 100)) 
             frame = padronizar_imagem(frame)
-
+            frame = cv2.resize(frame, (400, 400)) 
             cv2.imshow("Video", frame)
         k = cv2.waitKey(30) & 0xff
         if k == 27:
